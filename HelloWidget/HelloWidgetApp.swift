28f86e77af9cbf91b93d71adb818d4547ba74f2b
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2024-01-07 21:06:05 -0500

    Initial Commit

---
+++ HelloWidget/HelloWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWidgetApp.swift
+//  HelloWidget
+//
+//  Created by Travis Luckenbaugh on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
